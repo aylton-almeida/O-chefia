2f449a38548e5f5a008bc6a5ddce4bbe99bb8fc2
stevreeper <38439416+stevreeper@users.noreply.github.com>
2018-10-02 18:43:15 -0300

    Comeco branch

---
+++ src/Restaurante.java
@@ -0,0 +1,12 @@
+import java.util.Dictionary;
+import java.util.List;
+
+public class Restaurante {
+    private String nome, cnpj;
+    private Endereco endereco;
+    private int nummesas;
+    private List<Object> cardapio;
+    private Dictionary comentarios, pedidosAbertos, historicoDePedidos;
+
+
+}
